9bd6387d1f5069b58e7a90541db467caf6f87ff8
Jeffrey Arnell <arnellj@byu.edu>
2021-03-10 17:04:39 -0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,6 +20,11 @@ import { ContactComponent } from './contact/contact.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgModule } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -9,9 +8,20 @@ export class ContactComponent implements OnInit {
   public name: string = "";
   public email: string = "";
   public message: string = "";
+  public subject: string = "";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  sendEmail(){
+    console.log(this.name);
+    console.log(this.email);
+    console.log(this.message);
+    console.log(this.subject);
+    this.name = "";
+    this.email = "";
+    this.message = "";
+    this.subject = "";
+  }
 }
